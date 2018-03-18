3555409be3b313c4d49c0f9c64169c05a3b16527
dotun-o <email@dotun.me>
2018-03-17 21:51:54 -0400

    Update color.ts

--- models/color.ts
+++ models/color.ts
@@ -4,9 +4,10 @@ class Color {
     b: number;
 
     constructor(r: number, g: number, b: number) {
-        this.r = r > 1 ? 1 : r;
-        this.g = g > 1 ? 1 : g;
-        this.b = b > 1 ? 1 : b;
+        // never exceed 1.0
+        this.r = r > 1.0 ? 1.0 : r;
+        this.g = g > 1.0 ? 1.0 : g;
+        this.b = b > 1.0 ? 1.0 : b;
     }
 
     static scale(m: number, c: Color) {
@@ -16,4 +17,6 @@ class Color {
     static WHITE = new Color(1.0, 1.0, 1.0);
     static GRAY = new Color(0.5, 0.5, 0.5);
     static BLACK = new Color(0.0, 0.0, 0.0);
+    static BACKGROUND = Color.BLACK;
+    static FOREGROUND = Color.WHITE;
 }
